5a905c263715deaa0ddd37d769a583117c7f1dff
Abraham Arce <xe1gyq@gmail.com>
2014-01-12 14:56:19 -0600

    Scripts: Applications Clock
    Get the system time and date
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ scripts/applications/clock.py
@@ -0,0 +1,17 @@
+import time
+from voicesynthetizer import Synthetizer
+
+class Clock():
+
+	def __init__(self):
+		self.speaker = Synthetizer("festival", "english")
+
+	def clockget(self):
+		self.speaker.speechit(time.strftime("%A %B %d %Y %H:%M:%S"))
+
+
+if __name__ == "__main__":
+
+	myclock = Clock()
+	myclock.clockget()
+
